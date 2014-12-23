81a7951c773a00f7c93bf8f983861be7bf3b80f3
buschidos <sergey.snjozyk@gmail.com>
2014-12-23 09:28:52 +0200

    new template for output scss file for spritesmith

--- gulpfile.js
+++ gulpfile.js
@@ -14,21 +14,21 @@ var gulp         = require('gulp'),
 
 //** src paths **
 var src = {
-    root : 'source',
-    jade : 'source/jade',
-    sass : 'source/sass',
-    js   : 'source/js',
-    img  : 'source/img'
+    root    : 'source',
+    jade    : 'source/jade/',
+    sass    : 'source/sass/',
+    js      : 'source/js/',
+    img     : 'source/img/',
+    helpers : 'source/helpers/'
 };
 
 //** dest paths **
 var dest = {
-    root    : 'build',
-    html    : 'build',
-    css     : 'build/css',
-    js      : 'build/js',
-    img     : 'build/img',
-    helpers : 'build/helpers'
+    root : 'build',
+    html : 'build',
+    css  : 'build/css',
+    js   : 'build/js',
+    img  : 'build/img'
 };
 
 
@@ -48,16 +48,16 @@ gulp.task('browser-sync', function() {
 });
 
 // generate sprite
-gulp.task('sprite', function () {
-    var spriteData = gulp.src(src.img + '/icons/*.png')
+gulp.task('sprite', function() {
+    var spriteData = gulp.src(src.img + 'icons/*.png')
     .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
     .pipe(spritesmith({
         imgName: 'icons.png',
-        cssName: '_sprite.sass',
+        cssName: '_sprite.scss',
         imgPath: '../img/icons.png',
         cssFormat: 'sass',
         padding: 10,
-        cssTemplate: src.helpers + '/sprite.template.mustache'
+        cssTemplate: src.helpers + 'sprite.template.mustache'
     }));
     spriteData.img
         .pipe(gulp.dest(dest.img));
@@ -67,21 +67,21 @@ gulp.task('sprite', function () {
 
 // compile sass with ruby-sass
 gulp.task('sass', function() {
-    return gulp.src(src.sass + '/**/*.sass')
+    return gulp.src(src.sass + '**/*.sass')
         .pipe(plumber({errorHandler: notify.onError(function(error){return error.message;})}))
         .pipe(sass({
             style: 'compact',
             'sourcemap=none': true
         }))
         .pipe(postcss([
-            autoprefixer({ browsers: ['> 0.5%'], cascade: false, map: true})
+            autoprefixer({ browsers: ['last 2 version'], cascade: false})
         ]))
         .pipe(gulp.dest(dest.css));
 });
 
 // compile jade
 gulp.task('jade', function() {
-    return gulp.src(src.jade + '/**/*.jade')
+    return gulp.src(src.jade + '**/*.jade')
         .pipe(plumber({errorHandler: notify.onError(function(error){return error.message;})}))
         .pipe(changed(dest.html, {extension: '.html'}))
         .pipe(jade({pretty: true}))
@@ -90,7 +90,7 @@ gulp.task('jade', function() {
 
 // optimize images
 gulp.task('imagemin', function() {
-    gulp.src([src.img, !src.img + '/icons/**'])
+    gulp.src([src.img, !src.img + 'icons/**'])
         .pipe(imagemin())
         .pipe(gulp.dest(dest.img));
 });
@@ -98,13 +98,15 @@ gulp.task('imagemin', function() {
 // watch files and run tasks
 gulp.task('watch', function() {
     // gulp.watch(src.sass + '/**/*.sass', ['compass']);
-    gulp.watch(src.sass + '/**/*.sass', ['sass']);
+    gulp.watch(src.sass + '**/*.sass', ['sass']);
     // gulp.watch(src.sass + '/**/*.sass', ['libsass']);
     // gulp.watch(dest.css, ['prefix']);
     // gulp.watch(src.js + '//**/.js', ['tasks']);
     // gulp.watch(src.img, ['imagemin']);
-    gulp.watch(src.img + '/icons/*', ['sprite']);
-    gulp.watch(src.jade + '/**/*.jade', ['jade']);
+    gulp.watch(src.img + 'icons/*', ['sprite']);
+    gulp.watch(src.jade + '**/*.jade', ['jade']);
 });
 
-gulp.task('default',['browser-sync','watch'], function() {});
+gulp.task('build', ['jade', 'sass', 'sprite'], function() {});
+
+gulp.task('default', ['browser-sync', 'watch'], function() {});
